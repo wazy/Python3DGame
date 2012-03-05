8eccab8a64c83ed3c917490751190ffe77875490
warrenonedge <warrenonedge@gmail.com>
2012-03-05 16:21:50 -0500

    Fixed helloWorld.py

--- Examples/Python Examples/Panda/helloWorld/helloWorld.py
+++ Examples/Python Examples/Panda/helloWorld/helloWorld.py
@@ -71,6 +71,7 @@ class MyApp(ShowBase):
         angleDegrees = task.time * 6.0
         angleRadians = angleDegrees * (pi / 180.0)
         self.camera.setPos(20 * sin(angleRadians), -20.0 * cos(angleRadians), 3)
+        self.camera.setHpr(angleDegrees,0,0)
         return Task.cont
 
  
